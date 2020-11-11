dbfe9ab3f5f9760fa3b4cb62efeafb43d04b7603
Simo Mäkelä <1053209+makes@users.noreply.github.com>
2020-11-11 02:22:54 +0200

    production config

--- runall.bat
+++ runall.bat
@@ -4,9 +4,9 @@ python process.py 3
 python process.py 4
 python process.py 5
 python process.py 6
-rem python report_visuals.py 1
-rem python report_visuals.py 2
-rem python report_visuals.py 3
-rem python report_visuals.py 4
-rem python report_visuals.py 5
-rem python report_visuals.py 6
+python report_visuals.py 1
+python report_visuals.py 2
+python report_visuals.py 3
+python report_visuals.py 4
+python report_visuals.py 5
+python report_visuals.py 6
